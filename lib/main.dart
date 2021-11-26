6bf44ef766270d4a6c7b90f0ace65ac7da132c40
Amarjit singh <amarjitsingh52922@gmail.com>
2021-11-26 15:14:12 +0530

    day2-4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firstapp/pages/home_page.dart';
 import 'package:firstapp/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,11 +15,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.red,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
       //  home: HomePage(),
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
         "/login": (context) => LoginPage(),
       },
     );
